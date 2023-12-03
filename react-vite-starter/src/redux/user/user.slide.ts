5429b95591f8eab4f2478745a943578aef11fccb
baothi <baothi246@gmail.com>
2023-12-03 15:10:16 +0700

    create user with redux

--- react-vite-starter/src/redux/user/user.slide.ts
+++ react-vite-starter/src/redux/user/user.slide.ts
@@ -4,23 +4,30 @@ import userService from './userService';
 // Define a type for your user data
 interface IUser {
   // Define user properties here
-    id: number;
-    name: string;
-    email: string;
+    id?: number;
+    name?: string;
+    email?: string;
 }
 
 // Define the state type
 interface UserState {
   listUsers: IUser[];
+  createUsers: IUser[];
   loading: boolean;
-  error: string | null;
+  isError: boolean,
+  isSuccess: boolean,
+  isCreateSuccess: boolean
+  
 }
 
 // Initial state with types
 const initialState: UserState = {
   listUsers: [],
+  createUsers: [],
   loading: false,
-  error: null,
+  isError: false,
+  isSuccess: false,
+  isCreateSuccess: false
 }
 
 // Async thunk with TypeScript
@@ -47,11 +54,37 @@ export const fetchListUsers = createAsyncThunk<
   }
 );
 
+export const fetchCreateUsers = createAsyncThunk(
+  'users/fetchCreateUsers',
+  async (newUser: IUser, thunkAPI) => {
+    try {
+      const response = await userService.fetchCreateNewUsers(newUser);
+      if(response && response.id){
+        thunkAPI.dispatch(fetchListUsers());
+      }
+      return response;
+    } catch (error) {
+      // Type check and cast the error
+      if (error instanceof Error) {
+        return thunkAPI.rejectWithValue(error.message);
+      } else {
+        // Handle cases where the error is not an instance of Error
+        // You might want to return a generic error message here
+        return thunkAPI.rejectWithValue('An unexpected error occurred');
+      }
+    }
+  }
+);
+
 // Slice with TypeScript
 const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {},
+  reducers: {
+    resetCreate(state){
+      state.isCreateSuccess = false;
+    }
+  },
   extraReducers: (builder) => {
     builder.addCase(fetchListUsers.pending, (state) => {
       state.loading = true;
@@ -59,12 +92,27 @@ const userSlice = createSlice({
     builder.addCase(fetchListUsers.fulfilled, (state, action: PayloadAction<IUser[]>) => {
       state.listUsers = action.payload;
       state.loading = false;
+      state.isSuccess = true;
     });
-    builder.addCase(fetchListUsers.rejected, (state, action: PayloadAction<string | undefined>) => {
-        state.error = action.payload ?? 'An unexpected error occurred'; // Provide a default error message
+    builder.addCase(fetchListUsers.rejected, (state) => {
+        state.isError= false,
+        state.loading = false;
+    });
+    builder.addCase(fetchCreateUsers.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchCreateUsers.fulfilled, (state, action: PayloadAction<IUser[]>) => {
+      state.createUsers = action.payload;
+      state.loading = false;
+      state.isCreateSuccess= true;
+    });
+    builder.addCase(fetchCreateUsers.rejected, (state) => {
+        state.isError= false,
         state.loading = false;
     });
   },
 });
+
+export const { resetCreate } = userSlice.actions;
 
 export default userSlice.reducer;
--- react-vite-starter/src/redux/user/userService.tsx
+++ react-vite-starter/src/redux/user/userService.tsx
@@ -1,3 +1,9 @@
+interface IUser {
+    name?: string | null;
+    email?: string | null;
+    // các trường khác nếu cần
+  }
+
 const fetchUsers = async () => {
     try {
         const res = await fetch("http://localhost:8000/users");
@@ -14,8 +20,34 @@ const fetchUsers = async () => {
     }
 };
 
+const fetchCreateNewUsers = async (user: IUser) => {
+    try {
+        console.log("Created new user from user data from")
+        console.log(user);
+        console.log("Created new user from user data from")
+        const res = await fetch("http://localhost:8000/users",{
+            method: 'POST',
+            body: JSON.stringify({
+                email: user.email,
+                name: user.name
+            })
+        });
+
+        if (!res.ok) {
+            throw new Error(`Error: ${res.status}`);
+        }
+
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error('There was an error fetching the users:', error);
+        throw error;
+    }
+};
+
 const userService = {
-    fetchUsers
+    fetchUsers,
+    fetchCreateNewUsers
 };
 
 export default userService;
